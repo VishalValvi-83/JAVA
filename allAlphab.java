61ba0b438e7de7fc80882683c5a6675b23192a8a
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-06-26 12:53:43 +0530

    Add implementation to print all alphabets from a to z

---
+++ allAlphab.java
@@ -0,0 +1,9 @@
+// Q. Write a program to print all the alphabets from a to z.
+
+public class allAlphab {
+    public static void main(String[] args) {
+        char ch = 'a';
+        while (ch <= 'z')
+            System.out.print(ch++ + " ");
+    }
+}
